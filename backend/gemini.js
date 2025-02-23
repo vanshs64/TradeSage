8a30c874ca4f126f88225507d3b58592b75d553a
Vicky Sekhon <114193273+VickySekhon@users.noreply.github.com>
2025-02-22 23:26:39 -0500

    Fine tune prompts

--- backend/gemini.js
+++ backend/gemini.js
@@ -15,7 +15,17 @@ async function get_gemini_response(tariff_code_input) {
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
     // what the model should receive as a prompt
-    const message = `Given a tariff code that represents a tariff imposed by the U.S. on an imported food item, explain what tariff the tariff code represents in a simple one-line response starting with "Tariff Code: ...represents". Here is the tariff code ${tariff_code_input}`;
+    const message = `Given a tariff code that represents a tariff imposed by the U.S. on an imported food item, explain what tariff the tariff code represents in a simple one-line response starting with "Tariff Code: ...represents". Here is the tariff code ${tariff_code_input}. If the tariff exists below then paraphrase it's contents.
+    
+    0809.10.00: This code is part of the Customs Tariff Schedule for edible fruit and nuts.
+
+    0802.52.00: Harmonized Tariff Schedule (HTS) code for shelled pistachios, fresh or dried. It can also refer to other nuts, fresh or dried, whether or not shelled or peeled. 
+
+    2202.10.00:  Harmonized Tariff Schedule (HTS) code for waters that are sweetened, flavored, or contain other sweetening agents. This includes mineral waters and aerated waters.
+
+    2611.00.60: Harmonized Tariff Schedule (HTS) code for tungsten ores and concentrates
+    
+    `;
 
     // what the model returns (ai generated response)
     const result = await model.generateContent(message);
